9c53778537fc95ee9405c1b2986c1f02efb88b76
nosi-ken <nosikenit@gmail.com>
2019-06-20 15:07:54 +0900

    基本的ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get '/rule',to: "static_pages#rule"
   resources :users
+
+  get '/login' , to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to:"sessions#destroy"
 end
