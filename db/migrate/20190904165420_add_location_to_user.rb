00294bf6057dfdae6cc0854bc687827397cd614b
nosi-ken <nosikenit@gmail.com>
2019-09-07 10:30:11 +0900

    さらにマイグレーション第るを消した

--- db/migrate/20190904165420_add_location_to_user.rb
+++ db/migrate/20190904165420_add_location_to_user.rb
@@ -1,7 +1,6 @@
 class AddLocationToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :location, :string
-    add_column :users, :birthday, :integer
     add_column :users, :introduction, :string
   end
 end
--- db/migrate/20190904184823_change_change.rb
+++
@@ -1,5 +0,0 @@
-class ChangeChange < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :birthday, :date
-  end
-end
